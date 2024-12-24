2383cc2b7cd8225b239f9e089253a840f0ce2d8e
Novian Andika <novian.andika@students.amikom.ac.id>
2024-12-24 13:25:52 +0700

    Add fetchHeroes and fetchHeroDetail functions; comment out API proxy configuration

--- src/services/FetchHeroes.tsx
+++ src/services/FetchHeroes.tsx
@@ -1,6 +1,12 @@
 import { Heroes } from "./Heroes";
 import { HeroDetail } from "./HeroDetail";
 
+/**
+ * Fetches a list of heroes from the API.
+ *
+ * @returns {Promise<Heroes[]>} A promise that resolves to an array of heroes.
+ * @throws Will log an error to the console and return an empty array if the fetch operation fails.
+ */
 export const fetchHeroes = async (): Promise<Heroes[]> => {
   try {
     const response = await fetch("/api/v1/heroes");
@@ -12,6 +18,13 @@ export const fetchHeroes = async (): Promise<Heroes[]> => {
   }
 };
 
+/**
+ * Fetches the details of a hero by their ID.
+ *
+ * @param {string} id - The ID of the hero to fetch details for.
+ * @returns {Promise<HeroDetail>} A promise that resolves to the hero's details.
+ * @throws Will throw an error if the fetch operation fails.
+ */
 export const fetchHeroDetail = async (id: string): Promise<HeroDetail> => {
   try {
     const response = await fetch(`/api/v1/heroes/${id}`);
